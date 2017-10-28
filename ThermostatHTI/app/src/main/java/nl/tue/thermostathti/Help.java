1f4b8120faa138d28e973d737a677303a00eaad8
phungdt <Dattheman@hotmail.com>
2017-10-29 00:44:09 +0200

    Added some sexy expandable text views in Help. C'est magnifique.

--- ThermostatHTI/app/src/main/java/nl/tue/thermostathti/Help.java
+++ ThermostatHTI/app/src/main/java/nl/tue/thermostathti/Help.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 public class Help extends AppCompatActivity {
 
@@ -44,6 +47,16 @@ public class Help extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ExpandableTextView expTv1 = (ExpandableTextView) findViewById(R.id.expand_text_viewQA1);
+        ExpandableTextView expTv2 = (ExpandableTextView) findViewById(R.id.expand_text_viewQA2);
+        ExpandableTextView expTv3 = (ExpandableTextView) findViewById(R.id.expand_text_viewQA3);
+        ExpandableTextView expTv4 = (ExpandableTextView) findViewById(R.id.expand_text_viewQA4);
+
+        expTv1.setText(Html.fromHtml(getString(R.string.QA1)));
+        expTv2.setText(Html.fromHtml(getString(R.string.QA2)));
+        expTv3.setText(Html.fromHtml(getString(R.string.QA3)));
+        expTv4.setText(Html.fromHtml(getString(R.string.QA4)));
     }
 
 }
